de1132763eae98a358c2e8eb683245eb2d9f345d
IsmaelP19 <ismaelperezortiz@gmail.com>
2023-08-03 12:19:14 +0200

    init(main): complete tutorial of django rest framework

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from snippets.views import SnippetViewSet, UserViewSet, api_root
+# from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
